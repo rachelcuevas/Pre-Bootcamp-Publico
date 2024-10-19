82fe1ca7e4908329f1ebc4a57c8e162f5865b376
rachelcuevas <rachel.cuevas.arriagada@gmail.com>
2024-10-18 21:49:27 -0300

    Agregado archivo Java: Informacion.java

---
+++ Informacion.java
@@ -0,0 +1,11 @@
+public class Informacion {
+    public static void main(String[] args) {
+        
+        System.out.println("Hola, mi nombre es Rachel Cuevas");
+        System.out.println("Tengo 24 años de edad");
+        System.out.println("Soy de Chile y vivo en la ciudad de Los Angeles");
+        System.out.println("Me gusta mucho programar y los videojuegos");
+    }
+
+
+}
